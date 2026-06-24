2ec9b6305da670e3050e29d43abdfb93d0354a36
Ran501 <rangdelchoney618@gmail.com>
2026-06-24 13:55:42 +0600

    fix thee logout route

--- cst_room_booking_system/src/app/api/logout/route.js
+++ cst_room_booking_system/src/app/api/logout/route.js
@@ -4,7 +4,7 @@ export async function POST() {
   const response = NextResponse.json({ success: true });
   response.cookies.set("session", "", {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
+    secure: process.env.HTTPS === "true",  
     sameSite: "lax",
     maxAge: 0,
     path: "/",
